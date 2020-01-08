848ae2873d6e9fb81f25f297ccbfedd2a4fb31fb
AkosCzifra <akosczifraa@gmail.com>
2020-01-08 16:33:40 +0100

    added room jses

---
+++ frontend-reservations/src/components/Room.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Room extends Component {
+  render() {
+    const { roomNumber, reserved } = this.props.room;
+
+    const roomStyle = {
+      boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+      transition: "0.3s",
+      backgroundColor: reserved ? "lightred" : "lightgreen",
+      display: "inline-flex",
+      padding: "1%",
+      margin: "1%"
+    };
+
+    const isReserved = reserved ? "yes" : "no";
+
+    return (
+      <div style={roomStyle}>
+        <p>{"Room Number: " + roomNumber + " Reserved: " + isReserved}</p>
+      </div>
+    );
+  }
+}
+
+//PropTypes
+Room.protoTypes = {
+  room: PropTypes.object.isRequired
+};
+
+export default Room;
