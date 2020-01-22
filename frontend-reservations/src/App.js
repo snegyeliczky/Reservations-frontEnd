681fa6cba864b4708392884aef10eaf3ed3b59e6
Sándor Negyeliczky <sandor.negyeliczky@gmail.com>
2020-01-22 17:01:47 +0100

    filterTime added

--- frontend-reservations/src/App.js
+++ frontend-reservations/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import GuestList from "./components/GuestList";
 import RoomList from "./components/RoomList";
-
+import SearchField from "./components/search/SearchField"
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { HotelProvider } from "./components/HotelContext";
 
@@ -15,6 +15,7 @@ function App() {
         <div className="App">
           <div className="container">
             <Nav />
+            <SearchField />
             <Route exact path="/" render={() => <GuestList />} />
             <Route exact path="/rooms" render={() => <RoomList />} />
           </div>
--- frontend-reservations/src/components/HotelContext.js
+++ frontend-reservations/src/components/HotelContext.js
@@ -1,13 +1,17 @@
 import React, { useState, createContext } from "react";
 import axios from "axios";
 
+
 export const HotelContext = createContext();
 
 export const HotelProvider = props => {
   const [guestList, setGuestList] = useState([]);
   const [roomList, setRoomList] = useState([]);
+  const [date, setDate] = useState(new Date())
+  const[filter, setFilter] = useState(false)
 
   async function fetchGuestList() {
+    setFilter(false)
     const result = await axios("/guest/all");
     setGuestList(result.data);
   }
@@ -17,10 +21,24 @@ export const HotelProvider = props => {
     setRoomList(result.data);
   };
 
+  const fetchForDate = async (updatedDate) =>{
+    setFilter(true)
+    setDate(updatedDate)
+    let month =
+        updatedDate.getMonth() + 1 > 10
+          ? updatedDate.getMonth() + 1
+          : "0" + (updatedDate.getMonth() + 1);
+      let day = updatedDate.getDate() > 10 ? updatedDate.getDate() : "0" + updatedDate.getDate();
+      let dateUrl = updatedDate.getFullYear() + "-" + month + "-" + day;
+    const url = `/guest/checkin?date=${dateUrl}`;
+    console.log(dateUrl)
+    axios.get(url).then(response => setGuestList(response.data))
+  }
+
   const updateGuestStatus = async (guestId, updatedGuestStatus) => {
     const url = `/guest/changestatus?id=${guestId}&status=${updatedGuestStatus}`;
     axios.put(url).then(response => {
-      fetchGuestList();
+      filter?fetchForDate(date):fetchGuestList();
     });
   };
 
@@ -31,7 +49,9 @@ export const HotelProvider = props => {
         fetchGuestList,
         roomList,
         fetchRoomList,
-        updateGuestStatus
+        updateGuestStatus,
+        date,
+        fetchForDate
       }}
     >
       {props.children}
