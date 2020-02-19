f6b59a0cdad385e790ed36dfd9b9a30ad34704b5
Melange93 <lakatos.reka.veronika@gmail.com>
2020-02-19 17:10:48 +0100

    New Form checkbox dont work

--- frontend-reservations/src/components/AddReservationForm.js
+++ frontend-reservations/src/components/AddReservationForm.js
@@ -6,20 +6,45 @@ import {HotelContext} from "./HotelContext";
 
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import DateTimePicker from "react-datetime-picker";
+import {DatePicker} from "antd";
+import 'antd/dist/antd.css';
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Checkbox from "@material-ui/core/Checkbox";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 const AddReservationForm = () => {
     const {register, handleSubmit, errors} = useForm();
     const {addNewGuest} = useContext(HotelContext);
-    const [checkInDate, setCheckInDate] = useState(new Date());
-    const [checkOutDate, setCheckOutDate] = useState(new Date());
+    const classes = useStyles();
     const [toHome, setToHome] = useState(false);
 
+    const [checkInDate, setCheckInDate] = useState(new Date());
+    const [checkOutDate, setCheckOutDate] = useState(new Date());
+    const [paymentMethod, setPaymentMethod] = useState();
+    const [checked, setChecked] = React.useState(false);
 
     const onSubmit = data => {
-        addNewGuest(data, checkInDate, checkOutDate);
-
-        setToHome(true);
+        console.log(data)
+        console.log(checkInDate)
+        console.log(checkOutDate)
+        console.log(paymentMethod)
+        console.log(checked)
+        //addNewGuest(data, checkInDate, checkOutDate);
+        //setToHome(true);
     };
 
     const handleCheckInChange = date => {
@@ -30,6 +55,15 @@ const AddReservationForm = () => {
         setCheckOutDate(date);
     };
 
+    const handleChangePayment = event => {
+        event.preventDefault();
+        setPaymentMethod(event.target.value);
+    };
+
+    const handleChangeTax = event => {
+        setChecked(event.target.checked);
+    };
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="form-background">
             <Typography variant="h6" gutterBottom>
@@ -37,25 +71,20 @@ const AddReservationForm = () => {
             </Typography>
             <TextField
                 required
-                id="name"
-                name="name"
-                label="Name"
+                id="firstname"
+                name="firstname"
+                label="First Name"
                 autoComplete="fname"
                 inputRef={register({required: true})}
             />
-            <br/>
-            <br/>
-            <br/>
-            <label>Check In Date</label>
-            {" "}
-            <label>Check Out Date</label>
-
-            <br/>
-            <DateTimePicker onChange={handleCheckInChange} value={checkInDate}/>
-            {" "}
-            <DateTimePicker onChange={handleCheckOutChange} value={checkOutDate}/>
-            <br/>
-            <br/>
+            <TextField
+                required
+                id="lastname"
+                name="lastname"
+                label="Last Name"
+                autoComplete="fname"
+                inputRef={register({required: true})}
+            />
             <TextField
                 required
                 id="email"
@@ -64,7 +93,6 @@ const AddReservationForm = () => {
                 autoComplete="fname"
                 inputRef={register({required: true})}
             />
-            {" "}
             <TextField
                 required
                 id="country"
@@ -99,6 +127,50 @@ const AddReservationForm = () => {
                 inputRef={register({required: true})}
             />
             <br/>
+            <br/>
+            <br/>
+            <label>Check In Date</label>
+            {" "}
+            <label>Check Out Date</label>
+
+            <br/>
+            <DatePicker onChange={handleCheckInChange}/>
+            {" "}
+            <DatePicker onChange={handleCheckOutChange}/>
+            <br/>
+            <TextField
+                required
+                id="price"
+                name="price"
+                label="Price"
+                type="number"
+                inputProps={{ min: "0"}}
+                autoComplete="fname"
+                inputRef={register({required: true})}
+            />{"EUR"}
+            <br/>
+            <FormControl required className={classes.formControl}>
+                <InputLabel id="payment-method-label">Payment Method</InputLabel>
+                <Select
+                    required
+                    labelId="payment-method-label"
+                    id="payment"
+                    name="payment-native-required"
+                    onChange={handleChangePayment}
+                >
+                    <MenuItem value={"CASH"}>CASH</MenuItem>
+                    <MenuItem value={"CARD"}>CARD</MenuItem>
+                    <MenuItem value={"TRANSFER"}>TRANSFER</MenuItem>
+                </Select>
+                <FormHelperText>Required</FormHelperText>
+            </FormControl>
+            <Checkbox
+                checked={checked}
+                onChange={handleChangeTax}
+                value="primary"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+
 
             {/* errors will return when field validation fails  */}
             {errors.exampleRequired && <span>This field is required</span>}
