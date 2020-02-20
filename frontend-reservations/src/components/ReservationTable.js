3448e9e275302b89107599be1c36764375b2c5f1
AkosCzifra <akosczifraa@gmail.com>
2020-02-20 10:15:06 +0100

    refactored reservation table

---
+++ frontend-reservations/src/components/ReservationTable.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect } from "react";
+import ReservationRow from "./ReservationRow";
+import { Table } from "react-bootstrap";
+import { HotelContext } from "./HotelContext";
+import SearchField from "./search/SearchField";
+
+const ReservationTable = () => {
+  const { reservationList, fetchForDate, fetchRoomList, date } = useContext(
+    HotelContext
+  );
+
+  useEffect(() => {
+    fetchForDate(date);
+    fetchRoomList();
+  }, []);
+
+  const divStyle = {
+    border: "1px solid black",
+    borderBottom: "1px solid #bfbdbd"
+  };
+
+  return (
+    <div id="reservation-table">
+      <SearchField />
+      <br />
+      <Table style={divStyle} className="table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>Room</th>
+            <th>Check In Date</th>
+            <th>Name</th>
+            <th>Check Out Date</th>
+            <th>Edit Status</th>
+            <th>Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {reservationList.map(reservation => (
+            <ReservationRow reservation={reservation} key={reservation.id} />
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default ReservationTable;
